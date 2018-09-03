74a867946aacf165fc8bea5321bbc17b03d1a607
John Martinez <jmartinez729@gmail.com>
2018-09-03 14:45:34 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,16 +1,10 @@
 def reverse_each_word(sentence)
   sentence_string = sentence.split
-  reverse_string = []
+  # reverse_string = []
   
-  sentence_string.each do |word|
-    
-    reverse_string << word.reverse
-  end 
+  # sentence_string.each do |word|
+  #   reverse_string << word.reverse
+  # end 
   
-  return reverse_string.join(" ") 
-    
-  
-  
-  
-  
+  # return reverse_string.join(" ") 
 end 
